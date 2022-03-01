fef568470578ed8e7e5d1cef88a70b8e6e3aef42
JoanR17 <joanr.m.1999@gmail.com>
2022-03-01 12:37:04 +0100

    Ej 1

--- PiezasProveedores/src/main/java/T26/PiezasProveedores/dao/IPiezasDAO.java
+++
@@ -1,9 +0,0 @@
-package T26.PiezasProveedores.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import T26.PiezasProveedores.dto.Piezas;
-
-
-public interface IPiezasDAO extends JpaRepository<Piezas, Long> {
-
-}
--- PiezasProveedores/src/main/java/T26/PiezasProveedores/dao/IProveedoresDAO.java
+++
@@ -1,9 +0,0 @@
-package T26.PiezasProveedores.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import T26.PiezasProveedores.dto.Proveedores;
-
-public interface IProveedoresDAO extends JpaRepository<Proveedores, String> {
-
-}
